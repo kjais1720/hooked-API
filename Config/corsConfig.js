1e6569d89e18a18fc5c2ecd5373cdd0d888d66bc
Krituraj Anand <jaiswalkrituraj20@gmail.com>
2022-06-23 20:27:58 +0530

    changing cors config

--- Config/corsConfig.js
+++ Config/corsConfig.js
@@ -1,11 +1,12 @@
 var whitelist = ['http://localhost:3000', 'http://192.168.43.159:3000', 'https://hooked-social.vercel.app']
 var corsConfig = {
   origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1 || origin) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
+    // if (whitelist.indexOf(origin) !== -1 || origin) {
+    //   callback(null, true)
+    // } else {
+    //   callback(new Error('Not allowed by CORS'))
+    // }
+    callback(null, true)
   },
   credentials:true
 }
