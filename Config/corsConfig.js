8990847d0c1b6e8c5ba89aff9b185ea8a726fbad
Krituraj Anand <jaiswalkrituraj20@gmail.com>
2022-06-23 19:46:42 +0530

    fix: removed corsConfig

--- Config/corsConfig.js
+++ Config/corsConfig.js
@@ -1,6 +1,7 @@
-var whitelist = ['http://localhost:3000', 'https://hooked-social.vercel.app']
+var whitelist = ['http://localhost:3000', 'http://192.168.43.159:3000', 'https://hooked-social.vercel.app']
 var corsConfig = {
   origin: function (origin, callback) {
+    console.log(origin)
     if (whitelist.indexOf(origin) !== -1 || !origin) {
       callback(null, true)
     } else {
