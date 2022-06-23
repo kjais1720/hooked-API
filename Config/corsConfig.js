4958e18af40c0b2c5b03293f359bd2c625722313
Krituraj Anand <jaiswalkrituraj20@gmail.com>
2022-06-23 19:22:44 +0530

    updated corsConfig

--- Config/corsConfig.js
+++ Config/corsConfig.js
@@ -2,5 +2,16 @@ const corsConfig = {
   origin: "https://hooked-social.vercel.app",
   credentials: true,
 };
+ 
+var whitelist = ['http://localhost:3000', 'https://hooked-social.vercel.app']
+var corsConfig = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
 
 export default corsConfig;
--- index.js
+++ index.js
@@ -18,7 +18,6 @@ app.use(fileUpload(
     tempFileDir: "/tmp",
   }
 ));
-app.use(cors(corsConfig))
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
@@ -26,6 +25,7 @@ cloudinary.config({
 });
 
 // Middleware
+app.use(cors(corsConfig))
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
