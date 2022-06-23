77707217031f27ec90a454f38f85fd3f71e295c7
Krituraj Anand <jaiswalkrituraj20@gmail.com>
2022-06-23 17:15:10 +0530

    Updadted origin URLs

--- Config/corsConfig.js
+++ Config/corsConfig.js
@@ -1,11 +1,6 @@
-
 const corsConfig = {
-  origin: [
-    "http://localhost:3000",
-    "http://localhost:3001",
-    "http://192.168.43.159:3000"
-  ],
+  origin: ["http://localhost:3000", "https://hooked-social.vercel.app"],
   credentials: true,
 };
 
-export default corsConfig
+export default corsConfig;
--- index.js
+++ index.js
@@ -29,7 +29,7 @@ cloudinary.config({
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 5000
 
 mongoose
   .connect(process.env.MONGO_DB, {
