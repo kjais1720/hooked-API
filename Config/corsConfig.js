49bf36009fdb12342f10bb696eb681cc54e1d77d
Krituraj Anand <jaiswalkrituraj20@gmail.com>
2022-06-23 22:23:59 +0530

    Added origin URL

--- Config/corsConfig.js
+++ Config/corsConfig.js
@@ -1,4 +1,4 @@
-var whitelist = ['http://localhost:3000', 'http://192.168.43.159:3000', 'https://hooked-social.vercel.app']
+var whitelist = ['http://localhost:3000', 'http://192.168.43.159:3000', 'https://hooked-social.vercel.app','https://hooked-social.vercel.app/']
 var corsConfig = {
   origin: function (origin, callback) {
     if (whitelist.indexOf(origin) !== -1 || origin) {
@@ -7,7 +7,6 @@ var corsConfig = {
       callback(new Error('Not allowed by CORS'))
     }
   },
-  credentials:true
 }
 
 export default corsConfig;
